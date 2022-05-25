76d5995438ac77168f2f265f48778175685868e9
orlando.tyrell <orlandotyrell@gmail.com>
2022-05-25 15:25:56 -0400

    catch up 3

--- main.js
+++ main.js
@@ -1,1 +1,38 @@
-console.log("Hello world");
+//console.log("Hello world");
+
+console.log ("HH","HH"); 
+console.log ("HH","HH"); 
+console.log ("HHHHHHH"); 
+console.log ("HH","HH"); 
+console.log ("HH","HH"); 
+
+console.log ("eee");
+console.log ("ee" );
+console.log ("eeeee");
+console.log ("eeeee");
+
+console.log ("lll", "lll");
+console.log ("lll", "lll");
+console.log ("lll", "lll");
+console.log ("lll", "lll"); 
+console.log ("lll", "lll");
+
+console.log  ("oooo");
+console.log ("oo","oo"); 
+console.log ("oo","oo");
+console.log  ("oooo"); 
+
+console.log  ("ww"  ,   "ww");
+console.log  ("ww"  ,   "ww");
+console.log   ("ww","ww","ww");
+console.log      ("ww","ww");
+
+console.log  ("oooo");
+console.log ("oo","oo"); 
+console.log ("oo","oo");
+console.log  ("oooo"); 
+
+console.log ("rr" , "rr")
+console.log ("rrr" ,    "r");
+console.log ("rr");
+console.log ("rr");
